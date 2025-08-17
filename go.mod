6b0cb69a7fa233e23245cb927ee95ea7baed4ecb
Daniel Furman <djfurman@gmail.com>
2025-08-16 20:26:03 -0400

    Setup a payment intent and get the backend started for Payment Intent

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/djfurman/webapp-go
 
 go 1.25.0
 
-require github.com/go-chi/chi/v5 v5.2.2 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+	github.com/stripe/stripe-go/v72 v72.122.0 // indirect
+)
---
+++ internal/card/card.go
@@ -0,0 +1,79 @@
+package card
+
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
+
+// Card Struct to hold Stripe API information
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
+}
+
+// Transaction struct to hold transaction details
+type Transaction struct {
+	TransactionStatusID int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BankReturnCode      string
+}
+
+// Charge a credit card
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.CreatePaymentIntent(currency, amount)
+}
+
+// CreatePaymentIntent term used by Stripe to create a credit charge
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+
+	// Create a payment intent
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	// params.AddMetadata("key", "value")
+
+	pi, err := paymentintent.New(params)
+	if err != nil {
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeErr.Code)
+		}
+
+		return nil, msg, err
+	}
+
+	return pi, "", nil
+}
+
+func cardErrorMessage(code stripe.ErrorCode) string {
+	var msg = ""
+
+	switch code {
+	case stripe.ErrorCodeCardDeclined:
+		msg = "Your card was declined"
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your card has expired"
+	case stripe.ErrorCodeIncorrectCVC:
+		msg = "Your card's security code is incorrect"
+	case stripe.ErrorCodeIncorrectZip:
+		msg = "Incorrect zip/postal code"
+	case stripe.ErrorCodeAmountTooLarge:
+		msg = "The amount is too large to charge to your card"
+	case stripe.ErrorCodeAmountTooSmall:
+		msg = "The amount is too small to charge to your card"
+	case stripe.ErrorCodeBalanceInsufficient:
+		msg = "Insufficient balance"
+	case stripe.ErrorCodePostalCodeInvalid:
+		msg = "Your postal code was invalid"
+	default:
+		msg = "Your card was declined."
+	}
+
+	return msg
+}
