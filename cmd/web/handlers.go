095f88de3ea93ea3cb04acb471972d2f5d674cb4
Daniel Furman <djfurman@gmail.com>
2025-08-17 10:40:11 -0400

    Display a very simple receipt.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,3 +7,33 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 		app.errorLog.Println(err)
 	}
 }
+
+func (app *application) Receipt(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// Read the posted data
+	cardHolder := r.Form.Get("cardholder-name")
+	email := r.Form.Get("cardholder-email")
+	paymentIntent := r.Form.Get("payment-intent")
+	paymentMethod := r.Form.Get("payment-method")
+	paymentAmount := r.Form.Get("payment-amount")
+	paymentCurrency := r.Form.Get("payment-currency")
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["pi"] = paymentIntent
+	data["pm"] = paymentMethod
+	data["pa"] = paymentAmount
+	data["pc"] = paymentCurrency
+
+	if err := app.renderTemplate(w, r, "receipt", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
