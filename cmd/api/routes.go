177ed464545d7e618010118b32ba0fd1f3fb46de
Daniel Furman <djfurman@gmail.com>
2025-08-16 20:33:53 -0400

    Add mux handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -4,10 +4,20 @@ import (
 	"net/http"
 
 	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	mux.Get("/api/payment-intent", app.GetPaymentIntent)
 	return mux
 }
